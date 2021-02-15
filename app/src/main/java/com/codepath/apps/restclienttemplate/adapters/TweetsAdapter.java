129f709217bf7ca04511680be18dbe8c406afefe
bsc24 <bchin08@gmail.com>
2021-02-14 20:35:04 -0500

    Added some log statements, made user profile picture into a circle, and made the "time since" have an "ago" following it. (So instead of showing something like "2h" it would show "2h ago")

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetsAdapter.java
@@ -1,22 +1,31 @@
-package com.codepath.apps.restclienttemplate;
+package com.codepath.apps.restclienttemplate.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.codepath.apps.restclienttemplate.DetailActivity;
+import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
 
+    public static final String TAG = "TweetsAdapter";
     Context context;
     List<Tweet> tweets;
 
@@ -30,6 +39,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
     @NonNull
     @Override
     public TweetsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder");
         View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
         return new ViewHolder(view);
     }
@@ -51,12 +61,14 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
 
     // Clean all ements of the recycler
     public void clear() {
+        Log.d(TAG, "clear");
         tweets.clear();
         notifyDataSetChanged();
     }
 
     // Add a list of items
     public void addAll(List<Tweet> tweetList) {
+        Log.d(TAG, "addAll");
         tweets.addAll(tweetList);
         notifyDataSetChanged();
     }
@@ -84,9 +96,10 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             tvBody.setText(tweet.getBody());
             tvScreenName.setText(tweet.getUser().getScreenName());
             tvName.setText("@" + tweet.getUser().getName());
-            tvTimestamp.setText(tweet.getFormattedTimestamp());
+            tvTimestamp.setText(tweet.getTimeSince() + " ago");
             Glide.with(context)
                     .load(tweet.getUser().getPublicImageUrl())
+                    .transform(new RoundedCorners(100))
                     .into(ivProfileImage);
         }
     }
